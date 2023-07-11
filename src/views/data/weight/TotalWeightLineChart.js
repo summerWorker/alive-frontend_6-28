a9f48066fc8b89d8decd5b913f9eee50b40ec8af
pp-hhh <yichen_X1@outlook.com>
2023-07-11 15:11:18 +0800

    weight view fix

--- src/views/data/weight/TotalWeightLineChart.js
+++ src/views/data/weight/TotalWeightLineChart.js
@@ -1,22 +1,43 @@
 // material-ui
-import { Button, CardContent, Grid, Typography } from '@mui/material';
+import {
+  Button,
+  CardContent, Dialog,
+  DialogActions, DialogContent,
+  DialogContentText, DialogTitle,
+  Grid,
+  IconButton,
+  TextField,
+  Typography,
+} from '@mui/material';
 
 import Chart from 'react-apexcharts';
 import getWeekChartData from './chart-data/weight-week-chart';
 import getMonthChartData from './chart-data/weight-month-chart';
-import getYearChartData from './chart-data/weight-year-chart';
+import {DatePicker} from 'antd';
+
+const { RangePicker } = DatePicker;
+import { Dropdown } from 'antd';
+import { FileAddFilled } from '@ant-design/icons';
 
 // project imports
 import MainCard from '../../../ui-component/cards/MainCard';
 import { gridSpacing } from '../../../store/constant';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import dayjs from "dayjs";
+import {DateCalendar} from "@mui/x-date-pickers";
+
+const infoFormat = 'YYYY-MM-DD';
 
 const TotalWeightLineChart = (props) => {
-  console.log(props);
-  // week month year data
-  // week: 0, month: 1, year: 2
-  const [timeValue, setTimeValue] = useState(1);
+  const [timeValue, setTimeValue] = useState(0);
   const handleChangeTime = (event, newValue) => {
+    const date = [dayjs(props.startTime), dayjs(props.endTime)];
+    if(newValue === 0) {
+      props.setStartTime(date[1].add(-6, 'd').format(infoFormat));
+
+    }else{
+      props.setMonthStartTime(date[1].add(-29, 'd').format(infoFormat));
+    }
     setTimeValue(newValue);
   };
 
@@ -27,29 +48,47 @@ const TotalWeightLineChart = (props) => {
   const today = new Date();
   const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
   for (let i = 0; i < 7; ++i) {
-    weekCate[6 - i] = weekDays[(today.getDay() - i + 7) % 7];
+    const cur_date = new Date(props.endTime);
+    cur_date.setDate(cur_date.getDate() - i);
+    // console.log(cur_date);
+    weekCate[6 - i] = weekDays[cur_date.getDay()];
   }
+  // console.log(weekCate);
   let temp_week = props.weekWeight;
+  // console.log(temp_week);
   for (let i = 0; i < temp_week.length; ++i) {
     const cur_date = new Date(temp_week[i].date);
+    const today = new Date(props.endTime);
     const day_of_week = cur_date.getDay() - today.getDay() + 6;
-    const cur_weight = temp_week[i].value;
+    const cur_weight = temp_week[i].weight;
     weekData[day_of_week] = cur_weight;
   }
+  // console.log(weekData);
 
   for (let i = 0; i < 30; ++i) {
-    const cur_month = (today.getMonth() + 1).toString().padStart(2, '0');
-    const cur_day = today.getDate().toString().padStart(2, '0');
+    const cur_date = new Date(props.endTime);
+    cur_date.setDate(cur_date.getDate() - i);
+    const cur_month = (cur_date.getMonth() + 1).toString().padStart(2, '0');
+    const cur_day = cur_date.getDate().toString().padStart(2, '0');
     monthCate[29 - i] = `${cur_month}-${cur_day}`;
   }
-  console.log(monthCate);
+  // console.log(monthCate);
+
   let temp_month = props.monthWeight;
+  // console.log(temp_month);
   for (let i = 0; i < temp_month.length; ++i) {
     const cur_date = new Date(temp_month[i].date);
-    const day_of_month = cur_date.getDate() - today.getDate() + 29;
-    const cur_weight = temp_month[i].value;
+    const today = new Date(props.endTime);
+    const day_of_month = 29 - (today.getTime() - cur_date.getTime()) / (1000 * 60 * 60 * 24);
+    // console.log(day_of_month);
+    const cur_weight = temp_month[i].weight;
     monthData[day_of_month] = cur_weight;
   }
+  // console.log(monthData);
+
+  //add weight
+
+
 
   return (
     <MainCard content={false}>
@@ -96,6 +135,23 @@ const TotalWeightLineChart = (props) => {
                 >
                   月
                 </Button>
+              </Grid>
+              <Grid item>
+                <RangePicker
+                    bordered={false}
+                    disabled={[true, false]}
+                    value={[dayjs(props.startTime), dayjs(props.endTime)]}
+                    onChange={(date) => {
+                      if(timeValue === 0) {
+                        props.setStartTime(date[1].add(-6, 'd').format(infoFormat));
+                        props.updateMonthData();
+                      }else {
+                        props.setMonthStartTime(date[1].add(-29, 'd').format(infoFormat));
+                        props.updateMonthData();
+                      }
+                      props.setEndTime(date[1].format(infoFormat));
+                    }}
+                />
               </Grid>
             </Grid>
           </Grid>
